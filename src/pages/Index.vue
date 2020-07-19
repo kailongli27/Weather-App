e507971d06af54cc7441eddda93094fdc07cf356
kailongli27 <55965406+kailongli27@users.noreply.github.com>
2020-07-19 02:08:45 -0400

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,36 +1,75 @@
 <template>
-  <q-page class="column">
+  <q-page class="column bg-grey-4">
     <div class="column text-center bg-orange-13">
-      <div class="text-h3 text-weight-light text-white q-mt-lg">
+      <div class="self-center text-h3 text-weight-light text-white q-mt-lg">
         Weather Check
       </div>
-      <q-input rounded standout="bg-orange-14" v-model="text" placeholder="Enter a city name" dense class="q-mt-lg q-mb-sm self-center text-black" style="width: 35%">
+      <q-input rounded standout="bg-orange-5" v-model="city" @keyup.enter="getWeatherData()" placeholder="Enter a city name" dense input-class="text-black" class="q-mt-lg q-mb-sm self-center text-body1" style="width: 35%">
         <template v-slot:prepend>
-          <q-icon name="place"></q-icon>
+          <q-icon name="place" class="text-black"></q-icon>
         </template>
         <template v-slot:append>
-          <q-icon name="search" @click="text = ''" class="cursor-pointer"></q-icon>
+          <q-icon name="search" @click="getWeatherData()" class="cursor-pointer text-black"></q-icon>
         </template>
       </q-input>
-      <q-toggle
-        v-model="third"
-        checked-icon="check"
-        color="green"
-        unchecked-icon="clear"
-        class="self-center"
-      ></q-toggle>
+      <q-btn-toggle
+        v-model="unitSelection"
+        class="self-center q-mt-md q-mb-md"
+        rounded
+        ripple
+        unelevated
+        toggle-color="blue-grey-5"
+        color="orange-12"
+        text-color="white"
+        toggle-text-color="white"
+        :options="[
+          {label: 'C', value: 'metric'},
+          {label: 'F', value: 'imperial'}
+        ]"
+      />
+    </div>
+    <div class="self-center q-mt-xl">
+      <q-card class="rounded q-pa-md q-mt-lg text-center">
+        <div>{{ this.queryCity.name }}, {{this.queryCity.country}}</div>
+        <div>{{ this.queryCity.temp }}</div>
+      </q-card>
     </div>
   </q-page>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'WeatherPage',
   data () {
     return {
-      text: '',
-      third: false,
-      weatherKey: 'ad0ddaff0572e83a6fcf96cf7a2e3ade'
+      city: '',
+      unitSelection: 'metric',
+      weatherAPI: {
+        key: 'ad0ddaff0572e83a6fcf96cf7a2e3ade'
+      },
+      queryCity: {
+        name: null,
+        country: null,
+        temp: null
+      }
+    }
+  },
+  methods: {
+    getWeatherData () {
+      console.log(this.city)
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.unitSelection}&appid=${this.weatherAPI.key}`)
+        .then((res) => {
+          console.log(res)
+          this.queryCity.name = res.data.name
+          this.queryCity.country = res.data.sys.country
+          this.queryCity.temp = res.data.main.temp
+        })
+        .catch(() => {
+          alert('City not found!')
+          console.log('City not found!')
+        })
     }
   }
 }
